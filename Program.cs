9a6327757c3474c07c92f198497e65b7f63c7f31
Daniel Correa Lobato <daniel@lobato.org>
2024-09-18 16:09:27 -0300

    Implementação inicial

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using IDConsole.Exemplo;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddTransient<ITransientService, ServicoTransiente>();
+builder.Services.AddScoped<IScopedService, ServicoScoped>();
+builder.Services.AddSingleton<ISingletonService, ServicoSingleton>();
+
+builder.Services.AddTransient<ServicoMostrarCicloDeVida>();
+
+using var host = builder.Build();
+
+ExemploCicloDeVida(host.Services, "Ciclo de vida 1");
+ExemploCicloDeVida(host.Services, "Ciclo de vida 2");
+
+await host.RunAsync();
+return;
+
+static void ExemploCicloDeVida(IServiceProvider hostProvider, string lifetime)
+{
+    using IServiceScope serviceScope = hostProvider.CreateScope();
+    IServiceProvider provider = serviceScope.ServiceProvider;
+    ServicoMostrarCicloDeVida logger = provider.GetRequiredService<ServicoMostrarCicloDeVida>();
+
+    logger.MostrarDetalhes(
+        $"{lifetime}: Chamada 1 para ServicoMostrarCicloDeVida()");
+
+    Console.WriteLine("...");
+
+    logger = provider.GetRequiredService<ServicoMostrarCicloDeVida>();
+    logger.MostrarDetalhes(
+        $"{lifetime}: Chamada 1 para ServicoMostrarCicloDeVida()");
+
+    Console.WriteLine();
+}
